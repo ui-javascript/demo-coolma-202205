28b120043b0c12d97675c4f51ecab124708a45cb
luo0412 <1095847440@qq.com>
2022-05-12 03:47:12 +0000

    adjust

--- _directive/utils/utils.js
+++ _directive/utils/utils.js
@@ -17,8 +17,8 @@ export function initAliasMeta(annoAliasMeta, annoName, aliaName, config) {
 export function registerAnno(anno, annoAlias, node, ancestors) {
   let aliasAttributes = null;
 
+  // 判断当前注解是否合法
   if (node.name !== anno.namespace) {
-
     // 这个节点是否有注册别名
     if (Object.keys(annoAlias).includes(node.name) && annoAlias[node.name].attachAnno === anno.namespace) {
       aliasAttributes = annoAlias[node.name]['properties'];
@@ -26,14 +26,26 @@ export function registerAnno(anno, annoAlias, node, ancestors) {
       renderVoidElement(node)
       return;
     }
+  }
 
+  if (anno.beforeRender && anno.beforeRender.prevNode2Attr) {
+    anno.beforeRender.prevNode2Attr(node, ancestors)
+  }
+
+  if (anno.beforeRender && anno.beforeRender.nextNode2Attr) {
+    anno.beforeRender.nextNode2Attr(node, ancestors)
+  }
+
+  if (anno.beforeRender && anno.beforeRender.args2Attr) {
+    anno.beforeRender.args2Attr(node, ancestors)
   }
 
   if (aliasAttributes) {
     // @fix 小心aliasAttributes被覆盖
-    // 当前节点的属性优先级最高
+    // 配置属性优先级 args2Attr > nextNode2Attr > prevNode2Attr > node.attributes > aliasAttributes > 默认属性    
     node.attributes = Object.assign({}, aliasAttributes, node.attributes);
   }
 
+  // 开始渲染合法标签
   anno.render(node, ancestors);
 }
