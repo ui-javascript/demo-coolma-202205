ca003b527daacc1bde910ca1262be988fc5d0286
luo0412 <1095847440@qq.com>
2022-10-26 18:41:47 +0800

    Update index.js

--- _coolma/anno/@input/index.js
+++ _coolma/anno/@input/index.js
@@ -35,7 +35,8 @@ export default {
     const inputId = getNanoId()
     const data = node.data || (node.data = {});
     const hast = h(`span`, {
-      style: "display: inline-block"
+      style: "display: inline-block",
+      {...node.attributes}
     }, [  
       h(`span#${inputId}`, {...node.attributes}, '')
     ]);
